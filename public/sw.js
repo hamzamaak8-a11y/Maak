a48420af9662e9c3f93275ae612a04cf706afc61
hamzamaak8-a11y <hamzamaak8@gmail.com>
2026-09-07 22:01:50 +0100

    fix(pwa): invalidate stale Maak app shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "maak-shell-v11";
+const CACHE_NAME = "maak-shell-v12";
 const APP_SHELL = ["/Maak/","/Maak/index.html","/Maak/manifest.webmanifest","/Maak/404.html","/Maak/icon-192.png","/Maak/icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
